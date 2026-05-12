53dc740a2151041bd0552bc540aa4d97a5f9e5c0
Cibucya <s0684775482@gmail.com>
2026-05-12 11:14:37 -0400

    feat: Add Beard class with 2D grid initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+
+class Board {
+public:
+	size_t board_size;
+	std::vector<std::vector<int>> board;
+	Board(int size)
+		: board_size(size),
+		  board(size, std::vector<int>(size, 0)) {
+	}
+
+	void print_board() {
+
+	}
+};
 
 int main(void) {
-	std::cout << "Hello world!\n";
+
 
 	return 0;
 }
