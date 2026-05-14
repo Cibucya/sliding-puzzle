095785616ff0e574b705575977b2fc19369341d9
Cibucya <s0684775482@gmail.com>
2026-05-13 20:43:33 -0400

    feat: refactor board storage to 1d vector and add dimention support

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,58 @@
 #include <string>
 
 class Board {
+private:
+	size_t width;
+	size_t height;
+	std::vector<int> grid;
+
 public:
-	size_t board_size;
-	std::vector<std::vector<int>> board;
-	Board(int size)
-		: board_size(size),
-		  board(size, std::vector<int>(size, 0)) {
+	Board(int width, int height)
+		: width(width),
+		  height(height),
+		  grid(width * height, 0) {
+	}
+
+	auto at(int x, int y) {
+		return grid[y * width + x];
 	}
 
 	void print_board() {
+		std::string border(width + 2, '-');
+		std::cout << border << '\n';
 
+		for (int i = 0; i < height; ++i) {
+			std::cout << "|";
+			for (int j = 0; j < width; ++j) {
+				int val = at(j, i);
+				std::cout << (val == 0 ? ' ' : val);
+			}
+			std::cout << "|\n";
+		}
+		
+		std::cout << border << '\n';
 	}
 };
 
+void help() {
+	std::cout << "Usage: ./sliding <board_width> <board_height>" << std::endl;
+}
+
 int main(int argc, char** argv) {
-	int board_size = 4;
-	if (argc >= 2) {
+	int board_w = 0;
+	int board_h = 0;
+	if (argc >= 3) {
 		try {
-			board_size = std::stoi(argv[1]);
+			board_w = std::stoi(argv[1]);
+			board_h = std::stoi(argv[2]);
 		}
 		catch (const std::exception& e) {
-			std::cout << "Error: Board size has to be an integer.\n";
+			help();
 			return 1;
 		}
 	}
 
-	Board board(board_size);
+	Board board(board_w, board_w);
 
 	return 0;
 }
